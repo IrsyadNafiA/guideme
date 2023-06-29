65ce6f586b03f93e51a297ea6d7a9f10f4391744
Veve Karisa <karisaklaudia2003@gmail.com>
2023-06-30 02:11:39 +0700

    update halaman category, halaman detail dan add halaman seemore

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,38 +1,45 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
-        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
-        <style>
-            #map { height: 280px; }
-        </style>
-        
-        <title>{{ $title }} - GuideMe</title>
 
-        <!-- Fonts -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        @include('layouts.navbar')
-        @yield('main')
-        @include('layouts.footer')
-    </body>
-    <script src="{{ asset ('assets') }}/js/lib/jquery-3.4.1.min.js"></script>
-    <!-- Bootstrap-->
-    <script src="{{ asset ('assets') }}/js/lib/popper.min.js"></script>
-    <script src="{{ asset ('assets') }}/js/lib/bootstrap.min.js"></script>
-    <!-- Ionicons -->
-    <script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
-    <!-- Owl Carousel -->
-    <script src="{{ asset ('assets') }}/js/plugins/owl-carousel/owl.carousel.min.js"></script>
-    <!-- jQuery Circle Progress -->
-    <script src="{{ asset ('assets') }}/js/plugins/jquery-circle-progress/circle-progress.min.js"></script>
-    <!-- Base Js File -->
-    <script src="{{ asset ('assets') }}/js/base.js"></script>
-    @stack('mys')
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.6/flowbite.min.css" rel="stylesheet" />
+    <style>
+        #map {
+            height: 280px;
+        }
+    </style>
+
+    <title>{{ $title }} - GuideMe</title>
+
+    <!-- Fonts -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    @include('layouts.navbar')
+    @yield('main')
+    @include('layouts.footer')
+</body>
+<script src="{{ asset ('assets') }}/js/lib/jquery-3.4.1.min.js"></script>
+<!-- Bootstrap-->
+<script src="{{ asset ('assets') }}/js/lib/popper.min.js"></script>
+<script src="{{ asset ('assets') }}/js/lib/bootstrap.min.js"></script>
+<!-- Ionicons -->
+<script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
+<!-- Owl Carousel -->
+<script src="{{ asset ('assets') }}/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+<!-- jQuery Circle Progress -->
+<script src="{{ asset ('assets') }}/js/plugins/jquery-circle-progress/circle-progress.min.js"></script>
+<!-- Base Js File -->
+<script src="{{ asset ('assets') }}/js/base.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.6/flowbite.min.js"></script>
+@stack('mys')
+
 </html>
--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -4,11 +4,33 @@
 <div>
     <img class="w-full" src="{{ URL::to('/') }}/img/header_beach.png" alt="beach">
 </div>
-<div class="w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
+<div class="hover:bg-slate-100 w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
     <div class="w-[1280px] flex justify-between items-center py-5">
         <img src="{{ URL::to('/') }}/img/pantai 1.png" alt="ini gambar" class="w-[150px]">
         <p class="text-lg font-extrabold">Nuvasa beach, Nongsa</p>
-        <button class="bg-black text-white px-3 py-1 rounded-full">See More</button>
+        <a href="/seemore">
+            <button class="hover:bg-white hover:text-black hover:border-[1px] bg-black text-white px-3 py-1 rounded-full">See More</button>
+        </a>
+    </div>
+</div>
+<hr class="h-px bg-gray-300 border-0 dark:bg-gray-700">
+<div class="hover:bg-slate-100 w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
+    <div class="w-[1280px] flex justify-between items-center py-5">
+        <img src="{{ URL::to('/') }}/img/pantai 2.png" alt="ini gambar" class="w-[150px]">
+        <p class="text-lg font-extrabold">Nuvasa beach, Nongsa</p>
+        <a href="/seemore">
+            <button class="hover:bg-white hover:text-black hover:border-[1px] bg-black text-white px-3 py-1 rounded-full">See More</button>
+        </a>
+    </div>
+</div>
+<hr class="h-px bg-gray-300 border-0 dark:bg-gray-700">
+<div class="hover:bg-slate-100 w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
+    <div class="w-[1280px] flex justify-between items-center py-5">
+        <img src="{{ URL::to('/') }}/img/pantai 3.png" alt="ini gambar" class="w-[150px]">
+        <p class="text-lg font-extrabold">Nuvasa beach, Nongsa</p>
+        <a href="/seemore">
+            <button class="hover:bg-white hover:text-black hover:border-[1px] bg-black text-white px-3 py-1 rounded-full">See More</button>
+        </a>
     </div>
 </div>
 

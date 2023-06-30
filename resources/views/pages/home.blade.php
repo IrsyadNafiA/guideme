068cb6f46270414bee9dace578b6facefcb1c50c
Veve Karisa <karisaklaudia2003@gmail.com>
2023-06-30 09:24:14 +0700

    update halaman home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -22,16 +22,64 @@
     </div>
 </div>
 <h1 class="text-center my-7 text-4xl font-black drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">Arrival Location</h1>
-<div class="w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
+<div class="border hover:bg-slate-100 w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
     <div class="w-[1536px] flex justify-between items-center py-3 px-4">
         <div class="flex justify-start items-center gap-3 lg:gap-5 pr-2">
             <img src="{{ URL::to('/') }}/img/pelabuhan_batamcenter.jpeg" class="w-32 lg:w-56" alt="ini gambar">
             <p class="text-sm lg:text-lg font-extrabold">Batam Center Point</p>
         </div>
-        <button class="bg-black text-white px-2 text-base lg:text-lg lg:px-3 py-1 rounded-full hover:bg-black/80">See More</button>
+        <a href="/seemore">
+            <button class="hover:bg-white hover:text-black hover:border-[1px] bg-black text-white px-3 py-1 rounded-full">See More</button>
+        </a>
+    </div>
+</div>
+<div class="border hover:bg-slate-100 w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
+    <div class="w-[1536px] flex justify-between items-center py-3 px-4">
+        <div class="flex justify-start items-center gap-3 lg:gap-5 pr-2">
+            <img src="{{ URL::to('/') }}/img/pelabuhan_batamcenter.jpeg" class="w-32 lg:w-56" alt="ini gambar">
+            <p class="text-sm lg:text-lg font-extrabold">Batam Center Point</p>
+        </div>
+        <a href="/seemore">
+            <button class="hover:bg-white hover:text-black hover:border-[1px] bg-black text-white px-3 py-1 rounded-full">See More</button>
+        </a>
     </div>
 </div>
 <hr>
+<div class="items-center justify-center text-center">
+    <h1 class="mt-10 text-center text-4xl font-black drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">Iconic Places in Batam Island</h1>
+    <p class="mt-3 text-center">Find which destination you like to go by look out the category below</p>
+</div>
+
+
+<div class="w-full flex justify-center">
+    <div class="grid grid-cols-2 gap-14">
+        <div class="rounded-lg mt-5 flex text-center justify-center items-center relative">
+            <img class="w-[400px]" src="{{ URL::to('/') }}/img/wtb.png" alt="beach">
+            <a href="/detail" class="bg-white py-3 px-8 hover:bg-black hover:text-white rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                BEACH
+            </a>
+        </div>
+        <div class="rounded-lg mt-5 flex text-center justify-center items-center relative">
+            <img class="w-[400px]" src="{{ URL::to('/') }}/img/bb.png" alt="beach">
+            <a href="/detail" class="bg-white py-3 px-8 hover:bg-black hover:text-white rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                BEACH
+            </a>
+        </div>
+        <div class="rounded-lg flex text-center justify-center items-center relative">
+            <img class="w-[400px]" src="{{ URL::to('/') }}/img/ocarina.png" alt="beach">
+            <a href="/detail" class="bg-white py-3 px-8 hover:bg-black hover:text-white rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                BEACH
+            </a>
+        </div>
+        <div class="rounded-lg flex text-center justify-center items-center relative">
+            <img class="w-[400px]" src="{{ URL::to('/') }}/img/deer.png" alt="beach">
+            <a href="/detail" class="bg-white py-3 px-8 hover:bg-black hover:text-white rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                BEACH
+            </a>
+        </div>
+
+    </div>
+</div>
 
 @endsection
 @push('mys')

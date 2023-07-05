43eb5d9b2de98766b569b61ed5e70fa220a194b1
Irsyad Nafi <121683533+IrsyadNafiA@users.noreply.github.com>
2023-07-05 14:58:56 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,31 +18,34 @@
 
     <title>{{ $title }} - GuideMe</title>
 
-        <!-- Fonts -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        @include('layouts.navbar')
-        <div class="relative min-h-[100vh]">
-            <div class="pb-10">
-                @yield('main')
-            </div>
+    <!-- Fonts -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    @include('layouts.navbar')
+    <div class="relative min-h-[100vh]">
+        <div class="pb-10 w-full">
+            @yield('main')
         </div>
-        <footer class="w-full flex bg-[#1b1b1b] justify-center">
-            @include('layouts.footer')
-        </footer>
-    </body>
-    <script src="{{ asset ('assets') }}/js/lib/jquery-3.4.1.min.js"></script>
-    <!-- Bootstrap-->
-    <script src="{{ asset ('assets') }}/js/lib/popper.min.js"></script>
-    <script src="{{ asset ('assets') }}/js/lib/bootstrap.min.js"></script>
-    <!-- Ionicons -->
-    <script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
-    <!-- Owl Carousel -->
-    <script src="{{ asset ('assets') }}/js/plugins/owl-carousel/owl.carousel.min.js"></script>
-    <!-- jQuery Circle Progress -->
-    <script src="{{ asset ('assets') }}/js/plugins/jquery-circle-progress/circle-progress.min.js"></script>
-    <!-- Base Js File -->
-    <script src="{{ asset ('assets') }}/js/base.js"></script>
-    @stack('mys')
+    </div>
+    <footer class="w-full flex bg-[#1b1b1b] justify-center">
+        @include('layouts.footer')
+    </footer>
+</body>
+<script src="{{ asset ('assets') }}/js/lib/jquery-3.4.1.min.js"></script>
+<!-- Bootstrap-->
+<script src="{{ asset ('assets') }}/js/lib/popper.min.js"></script>
+<script src="{{ asset ('assets') }}/js/lib/bootstrap.min.js"></script>
+<!-- Ionicons -->
+<script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
+<!-- Owl Carousel -->
+<script src="{{ asset ('assets') }}/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+<!-- jQuery Circle Progress -->
+<script src="{{ asset ('assets') }}/js/plugins/jquery-circle-progress/circle-progress.min.js"></script>
+<!-- Base Js File -->
+<script src="{{ asset ('assets') }}/js/base.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
+@stack('mys')
+
 </html>

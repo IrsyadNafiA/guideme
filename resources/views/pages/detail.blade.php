208fdf334a394ad3da6f81a71088fcc6a87dd48f
Irsyad Nafi <121683533+IrsyadNafiA@users.noreply.github.com>
2023-07-12 14:09:39 +0700

    update detail page

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -1,37 +1,42 @@
 @extends('layouts.main')
 
 @section('main')
-<div>
-    <img class="w-full" src="{{ URL::to('/') }}/img/header_beach.png" alt="beach">
-</div>
-<div class="hover:bg-slate-100 w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
-    <div class="w-[1280px] flex justify-between items-center py-5">
-        <img src="{{ URL::to('/') }}/img/pantai 1.png" alt="ini gambar" class="w-[150px]">
-        <p class="text-lg font-extrabold">Nuvasa beach, Nongsa</p>
-        <a href="/seemore">
-            <button class="hover:bg-white hover:text-black hover:border-[1px] bg-black text-white px-3 py-1 rounded-full">See More</button>
-        </a>
+<div class="pt-16">
+    <div class="relative">
+        <img class="w-full object-cover h-80" src="{{ URL::to('/') }}/img/PANTAI.png" alt="beach">
+        <div class="absolute text-white text-center right-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <h1 class="font-black text-5xl">BEACH IN BATAM ISLAND</h1>
+            <p>Find which destination you like to go by look out the BEACH below</p>
+        </div>
+    </div>
+    <div class="hover:bg-slate-100 w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
+        <div class="w-[1280px] flex justify-between items-center py-5">
+            <img src="{{ URL::to('/') }}/img/pantai 1.png" alt="ini gambar" class="w-[150px]">
+            <p class="text-lg font-extrabold">Nuvasa beach, Nongsa</p>
+            <a href="/seemore">
+                <button class="hover:bg-white hover:text-black hover:border-[1px] bg-black text-white px-3 py-1 rounded-full">See More</button>
+            </a>
+        </div>
+    </div>
+    <hr class="h-px bg-gray-300 border-0 dark:bg-gray-700">
+    <div class="hover:bg-slate-100 w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
+        <div class="w-[1280px] flex justify-between items-center py-5">
+            <img src="{{ URL::to('/') }}/img/pantai 2.png" alt="ini gambar" class="w-[150px]">
+            <p class="text-lg font-extrabold">Nuvasa beach, Nongsa</p>
+            <a href="/seemore">
+                <button class="hover:bg-white hover:text-black hover:border-[1px] bg-black text-white px-3 py-1 rounded-full">See More</button>
+            </a>
+        </div>
+    </div>
+    <hr class="h-px bg-gray-300 border-0 dark:bg-gray-700">
+    <div class="hover:bg-slate-100 w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
+        <div class="w-[1280px] flex justify-between items-center py-5">
+            <img src="{{ URL::to('/') }}/img/pantai 3.png" alt="ini gambar" class="w-[150px]">
+            <p class="text-lg font-extrabold">Nuvasa beach, Nongsa</p>
+            <a href="/seemore">
+                <button class="hover:bg-white hover:text-black hover:border-[1px] bg-black text-white px-3 py-1 rounded-full">See More</button>
+            </a>
+        </div>
     </div>
 </div>
-<hr class="h-px bg-gray-300 border-0 dark:bg-gray-700">
-<div class="hover:bg-slate-100 w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
-    <div class="w-[1280px] flex justify-between items-center py-5">
-        <img src="{{ URL::to('/') }}/img/pantai 2.png" alt="ini gambar" class="w-[150px]">
-        <p class="text-lg font-extrabold">Nuvasa beach, Nongsa</p>
-        <a href="/seemore">
-            <button class="hover:bg-white hover:text-black hover:border-[1px] bg-black text-white px-3 py-1 rounded-full">See More</button>
-        </a>
-    </div>
-</div>
-<hr class="h-px bg-gray-300 border-0 dark:bg-gray-700">
-<div class="hover:bg-slate-100 w-full flex justify-center bg-white shadow-[0_1px_5px_-5px_rgba(0,0,0,0.3)]">
-    <div class="w-[1280px] flex justify-between items-center py-5">
-        <img src="{{ URL::to('/') }}/img/pantai 3.png" alt="ini gambar" class="w-[150px]">
-        <p class="text-lg font-extrabold">Nuvasa beach, Nongsa</p>
-        <a href="/seemore">
-            <button class="hover:bg-white hover:text-black hover:border-[1px] bg-black text-white px-3 py-1 rounded-full">See More</button>
-        </a>
-    </div>
-</div>
-
 @endsection
